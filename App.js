c137c6e1baa68658513059a10762487b43c2d585
Johnny Ryue <johnryue1993@gmail.com>
2022-04-30 15:02:56 -0700

    rollback changes so Expo app shows proper changes (dependencies issue)

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import {StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>first REACT NATIVE APP</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
