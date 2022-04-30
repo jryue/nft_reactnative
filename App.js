d0943b86cebc3546298703e700da350f48ce00c8
Johnny Ryue <johnryue1993@gmail.com>
2022-04-30 15:28:29 -0700

    commit 2

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import {StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from '/screen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>first REACT NATIVE APP</Text>
+    <View>
+      <Text>first REACT NATIVE APP sd sdsd </Text>
       <StatusBar style="auto"/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
