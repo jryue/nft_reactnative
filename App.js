74af6d7fae9e380de8a5047e87bac2f5f9ee4129
Johnny Ryue <johnryue1993@gmail.com>
2022-04-30 14:52:29 -0700

    initialization of app

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <View style={StyleSheet.container}>
-            <Text>Hello, World!</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>This is crazy simple</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
