665e1385366043c669eae440693e190f9e8e027d
Johnny Ryue <johnryue1993@gmail.com>
2022-04-30 16:22:38 -0700

    rearranged the assets and constants folder, deleted old asset folder

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,12 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import {useState } from 'react';
+import { View, SafeArea, FlatList, Text } from 'react-native';
+
+// FlatList renders items only when they are about to appear on screen; lowers memory usage
+// SafeArea component used to render content within safe area boundaries of a device; makes sure everything fits on screen
+// View is just the basic DIV component
+
+import {COLORS, NFTData } from '../constants';
 
 const Home = () => {
   return (
